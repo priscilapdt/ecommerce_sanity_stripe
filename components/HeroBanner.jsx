dfd7628b90e5309aa47f68ac91715c92b9226811
Priscila Pedrotti <priscila.pedrotti@outlook.com>
2022-12-22 13:57:14 -0300

    add image to Herbanner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,20 +1,22 @@
 import Link from "next/link";
+import { urlFor } from "../lib/client";
 
-const HeroBanner = () => {
+const HeroBanner = ({HeroBanner}) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">SMALL TEXT</p>
-        <h3>MID TEXT</h3>
-        <img src="" alt="headphones" className="hero-banner-image" />
+        <p className="beats-solo">{HeroBanner.smallText}</p>
+        <h3>{HeroBanner.midText}</h3>
+        <h1>{HeroBanner.largeText1}</h1>
+        <img src={urlFor(HeroBanner.image)} alt="headphones" className="hero-banner-image" />
       </div>
       <div>
-        <Link href="./product/ID">
-          <button type="button">BUTTON TEXT</button>
+        <Link href={`./product/${HeroBanner.product}`}>
+          <button type="button">{HeroBanner.buttonText}</button>
         </Link>
         <div className="desc">
           <h5>Description</h5>
-          <p>DESCRIPTION</p>
+          <p>{HeroBanner.desc}</p>
         </div>
       </div>
     </div>
