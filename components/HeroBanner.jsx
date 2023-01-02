2f63f8b6a9af582617f20deb8eaea54a07a216f7
Priscila Pedrotti <priscila.pedrotti@outlook.com>
2023-01-02 16:25:21 -0300

    add layout to the app page

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,22 +1,22 @@
 import Link from "next/link";
 import { urlFor } from "../lib/client";
 
-const HeroBanner = ({HeroBanner}) => {
+const HeroBanner = ({heroBanner}) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">{HeroBanner.smallText}</p>
-        <h3>{HeroBanner.midText}</h3>
-        <h1>{HeroBanner.largeText1}</h1>
-        <img src={urlFor(HeroBanner.image)} alt="headphones" className="hero-banner-image" />
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
       </div>
       <div>
-        <Link href={`./product/${HeroBanner.product}`}>
-          <button type="button">{HeroBanner.buttonText}</button>
+        <Link href={`./product/${heroBanner.product}`}>
+          <button type="button">{heroBanner.buttonText}</button>
         </Link>
         <div className="desc">
           <h5>Description</h5>
-          <p>{HeroBanner.desc}</p>
+          <p>{heroBanner.desc}</p>
         </div>
       </div>
     </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+
 const Navbar = () => {
-  return <div>oi</div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">E-commerce Headphones Store</Link>
+      </p>
+
+      <button type="button" className="card-icon" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
 };
 
 export default Navbar;
