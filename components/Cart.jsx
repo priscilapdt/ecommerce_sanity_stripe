dc23b85554341f93f06c185822ee15e4961b3be2
Priscila Pedrotti <priscila.pedrotti@outlook.com>
2023-01-20 13:12:49 -0300

    logic for cart

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -20,6 +20,7 @@ const Cart = () => {
     cartItems,
     setShowCart,
     toggleCartItemQuantity,
+    onRemove,
   } = useStateContext();
   return (
     <div className="cart-wrapper" ref={cartRef}>
@@ -84,7 +85,11 @@ const Cart = () => {
                         </span>
                       </p>
                     </div>
-                    <button type="button" className="remove-item" onClick="">
+                    <button
+                      type="button"
+                      className="remove-item"
+                      onClick={() => onRemove(item)}
+                    >
                       <TiDeleteOutline />
                     </button>
                   </div>
